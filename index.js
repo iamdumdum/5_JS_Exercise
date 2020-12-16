157d1a42cbd9f9e88468b42889d519f35e4d7a13
iamdumdum <Trevorjiang@protonmail.com>
2020-12-16 21:59:08 +0000

    done

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var greet = "Hello";
 var name = "Bruce";
 
 // only change code below this line
-
-var str = "";
-
+var here = "there";
+var comma = ",";
+var exclamation = "!";
+var str = greet + " " + here + comma + " " +
+ name + exclamation;
 // only change code above this line
 
 module.exports = { str }
